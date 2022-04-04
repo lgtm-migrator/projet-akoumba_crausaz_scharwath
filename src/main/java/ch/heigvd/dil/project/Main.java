6597dc2a32044fab2c810ea3dea10669f816befc
Maxime Scharwath <maxscharwath@gmail.com>
2022-04-04 16:39:23 +0200

    Test lint

--- src/main/java/ch/heigvd/dil/project/Main.java
+++ src/main/java/ch/heigvd/dil/project/Main.java
@@ -23,11 +23,12 @@ import picocli.CommandLine.Command;
 public class Main implements Callable<Integer> {
 
     public static void main(String[] args) {
-        System.exit(new CommandLine(new Main()).execute(args));
+        
+            
+            System.exit(new CommandLine(new Main()).execute(args));
     }
 
     @Override
-    public Integer call() {
-        return 0;
+    public Integer call() {return 0;
     }
 }
