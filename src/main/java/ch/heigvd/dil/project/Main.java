1ae5303cd4ea6a875fc97cc26b85d66abfe36bf8
nicrausaz <n.crausaz99@gmail.com>
2022-04-06 23:01:19 +0200

    Build command done

--- src/main/java/ch/heigvd/dil/project/Main.java
+++ src/main/java/ch/heigvd/dil/project/Main.java
@@ -1,14 +1,13 @@
 package ch.heigvd.dil.project;
 
-import static ch.heigvd.dil.project.FilesManager.FileManager.parseFile;
-import static ch.heigvd.dil.project.FilesManager.FileManager.parserMarkdownToHtml;
-
 import ch.heigvd.dil.project.commands.BuildCommand;
 import ch.heigvd.dil.project.commands.CleanCommand;
 import ch.heigvd.dil.project.commands.InitCommand;
 import ch.heigvd.dil.project.commands.ServeCommand;
-import java.io.*;
+
 import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(
@@ -17,43 +16,15 @@ import picocli.CommandLine.Command;
         description = "DIL project",
         versionProvider = ManifestVersionProvider.class,
         subcommands = {
-            InitCommand.class,
-            CleanCommand.class,
-            BuildCommand.class,
-            ServeCommand.class
+                InitCommand.class,
+                CleanCommand.class,
+                BuildCommand.class,
+                ServeCommand.class
         })
 public class Main implements Callable<Integer> {
 
-    public static void main(String[] args) throws IOException {
-        // System.exit(new CommandLine(new Main()).execute(args));
-
-        File file =
-                new File(
-                        new File(".").getCanonicalPath()
-                                + File.separator
-                                + "src"
-                                + File.separator
-                                + "main"
-                                + File.separator
-                                + "java"
-                                + File.separator
-                                + "ch"
-                                + File.separator
-                                + "heigvd"
-                                + File.separator
-                                + "dil"
-                                + File.separator
-                                + "project"
-                                + File.separator
-                                + "test.md");
-
-        System.out.println(file);
-        String yml = parseFile(file)[0];
-        String markdown = parseFile(file)[1];
-        System.out.println(yml);
-        System.out.println(markdown);
-        String s = parserMarkdownToHtml(markdown);
-        System.out.println(s);
+    public static void main(String[] args) {
+        System.exit(new CommandLine(new Main()).execute(args));
     }
 
     @Override
