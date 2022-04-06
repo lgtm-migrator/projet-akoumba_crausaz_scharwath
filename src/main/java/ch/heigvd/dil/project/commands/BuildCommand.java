8db25fbf32e69212950e404aa83fd2654610b62b
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-04-06 22:38:49 +0200

    base of build command

--- src/main/java/ch/heigvd/dil/project/commands/BuildCommand.java
+++ src/main/java/ch/heigvd/dil/project/commands/BuildCommand.java
@@ -1,12 +1,32 @@
 package ch.heigvd.dil.project.commands;
 
+import ch.heigvd.dil.project.FilesManager.TreeBuilder;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
+
+import java.io.File;
+import java.io.IOException;
 
 /** This class represents the command line interface for the build command. */
 @Command(name = "build", description = "Build sub-command", version = "1.0")
 public class BuildCommand implements Runnable {
+
+    @CommandLine.Parameters(
+            index = "0",
+            description = "Path to the site to build")
+    String creationPath;
+
     @Override
     public void run() {
-        System.out.println("Build command is not implemented yet.");
+        // Check if the source projet exists
+        if (!new File(creationPath).exists()) return;
+
+        // Build the site into build folder
+        var treeBuilder = new TreeBuilder(new File(creationPath), new File(creationPath + "/build"));
+        try {
+            treeBuilder.build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
