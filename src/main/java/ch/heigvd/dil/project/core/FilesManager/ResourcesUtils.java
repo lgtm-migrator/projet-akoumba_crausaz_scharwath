55c8d138b89ca19075653cf84a113d6bb87dda20
nicrausaz <nicrausaz@users.noreply.github.com>
2022-05-04 19:42:36 +0000

    Apply lint fixes

--- src/main/java/ch/heigvd/dil/project/core/FilesManager/ResourcesUtils.java
+++ src/main/java/ch/heigvd/dil/project/core/FilesManager/ResourcesUtils.java
@@ -17,18 +17,21 @@ public class ResourcesUtils {
 
     /**
      * Copy a resource to a destination
+     *
      * @param sourcePath wanted ressource
      * @param target path to the destination
      * @throws URISyntaxException if ressource not found
      * @throws IOException if destination path not found
      */
-    public static void copyFromJar(final String sourcePath, final Path target) throws URISyntaxException, IOException {
+    public static void copyFromJar(final String sourcePath, final Path target)
+            throws URISyntaxException, IOException {
         Path source = getPathFromJar(sourcePath);
         Files.walkFileTree(
                 source,
                 new SimpleFileVisitor<>() {
                     @Override
-                    public FileVisitResult preVisitDirectory(final Path dir, final BasicFileAttributes attrs) throws IOException {
+                    public FileVisitResult preVisitDirectory(
+                            final Path dir, final BasicFileAttributes attrs) throws IOException {
                         Path currentTarget = target.resolve(source.relativize(dir).toString());
                         Files.createDirectories(currentTarget);
                         return FileVisitResult.CONTINUE;
@@ -48,6 +51,7 @@ public class ResourcesUtils {
 
     /**
      * Get path to a resource
+     *
      * @param sourcePath wanted ressource
      * @return path of ressource
      * @throws URISyntaxException if ressource not found
@@ -55,9 +59,10 @@ public class ResourcesUtils {
     public static Path getPathFromJar(final String sourcePath) throws URISyntaxException {
         Path source;
         try {
-            FileSystem fileSystem = FileSystems.newFileSystem(
-                    ResourcesUtils.class.getResource("").toURI(),
-                    Collections.<String, String>emptyMap());
+            FileSystem fileSystem =
+                    FileSystems.newFileSystem(
+                            ResourcesUtils.class.getResource("").toURI(),
+                            Collections.<String, String>emptyMap());
             source = fileSystem.getPath(sourcePath);
         } catch (Exception e) {
             source = Path.of(ResourcesUtils.class.getClassLoader().getResource(sourcePath).toURI());
@@ -67,6 +72,7 @@ public class ResourcesUtils {
 
     /**
      * Get a resource file content as string
+     *
      * @param sourcePath wanted ressource
      * @return resource file content as string
      * @throws URISyntaxException if ressource not found
