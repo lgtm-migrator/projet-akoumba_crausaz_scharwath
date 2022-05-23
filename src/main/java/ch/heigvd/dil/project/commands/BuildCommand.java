998c3f85b9efe2e5bc37ebef48841dbcde6f1efd
Maxime Scharwath <maxscharwath@gmail.com>
2022-05-24 00:08:41 +0200

    Add FilesWatcher

--- src/main/java/ch/heigvd/dil/project/commands/BuildCommand.java
+++ src/main/java/ch/heigvd/dil/project/commands/BuildCommand.java
@@ -1,8 +1,12 @@
 package ch.heigvd.dil.project.commands;
 
+import ch.heigvd.dil.project.core.FilesManager.FilesWatcher;
+import ch.heigvd.dil.project.core.FilesManager.FilesWatcherHandler;
 import ch.heigvd.dil.project.core.FilesManager.TreeBuilder;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
 import java.util.logging.Logger;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -23,6 +27,9 @@ public class BuildCommand extends BaseCommand {
 
     @CommandLine.Parameters(index = "0", description = "Path to the site to build")
     String creationPath;
+
+    @CommandLine.Option(names = {"-w", "--watch"}, description = "Watch the site for changes")
+    boolean watch;
 
     @Override
     protected String getRootPath() {
@@ -46,6 +53,28 @@ public class BuildCommand extends BaseCommand {
         } catch (IOException e) {
             LOG.severe("An error occurred while building the site: " + e.getMessage());
             e.printStackTrace();
+        }
+
+        if(watch) {
+            try {
+                var watcher = new FilesWatcher(srcDir.toPath(), new FilesWatcherHandler() {
+                    @Override
+                    public void onChange(Path path, WatchEvent.Kind<?> kind) {
+                        LOG.info("File " + path.toAbsolutePath() + " has changed");
+                        try {
+                            treeBuilder.build();
+                        } catch (IOException e) {
+                            LOG.severe("An error occurred while building the site: " + e.getMessage());
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                watcher.addIgnoreFile("build");
+                watcher.watch();
+            } catch (Exception e) {
+                LOG.severe("An error occurred while watching the build folder: " + e.getMessage());
+                e.printStackTrace();
+            }
         }
     }
 }
