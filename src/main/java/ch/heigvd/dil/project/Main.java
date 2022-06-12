7f1fefeec39515721625e69d581587cd92d4daca
nicrausaz <n.crausaz99@gmail.com>
2022-06-12 21:34:33 +0200

    Comments

--- src/main/java/ch/heigvd/dil/project/Main.java
+++ src/main/java/ch/heigvd/dil/project/Main.java
@@ -4,19 +4,32 @@ import ch.heigvd.dil.project.commands.*;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+/**
+ * Main entry point for the application
+ *
+ * @author Akoumba Ludivine
+ * @author Crausaz Nicolas
+ * @author Scharwath Maxime
+ */
 @Command(
         mixinStandardHelpOptions = true,
         name = "dil",
         description = "DIL project",
         versionProvider = ManifestVersionProvider.class,
         subcommands = {
-            InitCommand.class,
-            CleanCommand.class,
-            BuildCommand.class,
-            ServeCommand.class,
-            PublishCommand.class
+                InitCommand.class,
+                CleanCommand.class,
+                BuildCommand.class,
+                ServeCommand.class,
+                PublishCommand.class
         })
 public class Main {
+
+    /**
+     * Executes the wished command
+     *
+     * @param args command and args
+     */
     public static void main(String[] args) {
         new CommandLine(new Main()).execute(args);
     }
